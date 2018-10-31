8bbc8971fd062b83500ddab367a43081725e8bc7
Quinlan Jung <quin@expo.io>
2018-10-30 19:10:24 -0700

    fixing cli

--- packages/expo-detox-cli/cli.js
+++ packages/expo-detox-cli/cli.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 const cp = require('child_process');
 const path = require('path');
 const fs = require('fs');
@@ -8,8 +7,19 @@ const os = require('os');
 const OSX_APP_PATH = path.join(os.homedir(), 'Library');
 const OSX_LIBRARY_ROOT_PATH = path.join(OSX_APP_PATH, 'ExpoDetoxHook');
 
+const detoxAppPath = path.join(process.cwd(), 'node_modules/detox');
+const detoxPackageJsonPath = path.join(detoxAppPath, 'package.json');
+
+const expoDetoxHookAppPath = path.join(process.cwd(), 'node_modules/expo-detox-hook');
+const expoDetoxHookPackageJsonPath = path.join(expoDetoxHookAppPath, 'package.json');
+
 function getVersion() {
-    return require(path.join(__dirname, 'package.json')).version;
+    if (!fs.existsSync(expoDetoxHookPackageJsonPath)) {
+        console.log("expo-detox-hook is not installed in this directory");
+        process.exit(1);
+    }
+
+    return require(expoDetoxHookPackageJsonPath).version;
   }
 
 function getFrameworkPath() {
@@ -18,32 +28,31 @@ function getFrameworkPath() {
     return `${OSX_LIBRARY_ROOT_PATH}/ios/${sha1}/ExpoDetoxHook.framework/ExpoDetoxHook`;
 }
 
-const detoxAppPath = path.join(process.cwd(), 'node_modules/detox');
-const detoxPackageJsonPath = path.join(detoxAppPath, 'package.json');
-
-if (fs.existsSync(detoxPackageJsonPath)) {
-    // { shell: true } option seems to break quoting on windows? Otherwise this would be much simpler.
-    if (process.platform === 'win32') {
-        const result = cp.spawnSync(
-        'cmd',
-        ['/c', path.join(process.cwd(), 'node_modules/.bin/detox.cmd')].concat(process.argv.slice(2)),
-        { stdio: 'inherit' });
-        process.exit(result.status);
-    } else {
-        const expoDetoxHookFrameworkPath = getFrameworkPath();
-        if (!fs.existsSync(expoDetoxHookFrameworkPath)){
-            console.log("expo-detox-hook is not installed in your osx Library. See <TODO:(quin) readme> for more info.");
-            process.exit(1);
-        }
-        const result = cp.spawnSync(
-        path.join(process.cwd(), 'node_modules/.bin/detox'),
-        process.argv.slice(2),
-        { stdio: 'inherit',
-          env: {SIMCTL_CHILD_DYLD_INSERT_LIBRARIES: expoDetoxHookFrameworkPath},
-        });
-        process.exit(result.status);
-    }
-} else {
+if (!fs.existsSync(detoxPackageJsonPath)) {
     console.log("detox is not installed in this directory");
     process.exit(1);
+}
+
+// { shell: true } option seems to break quoting on windows? Otherwise this would be much simpler.
+if (process.platform === 'win32') {
+    const result = cp.spawnSync(
+    'cmd',
+    ['/c', path.join(process.cwd(), 'node_modules/.bin/detox.cmd')].concat(process.argv.slice(2)),
+    { stdio: 'inherit' });
+    process.exit(result.status);
+} else {
+    const expoDetoxHookFrameworkPath = getFrameworkPath();
+    if (!fs.existsSync(expoDetoxHookFrameworkPath)){
+        console.log("expo-detox-hook is not installed in your osx Library. See <TODO:(quin) readme> for more info.");
+        process.exit(1);
+    }
+    const result = cp.spawnSync(
+    path.join(process.cwd(), 'node_modules/.bin/detox'),
+    process.argv.slice(2),
+    { stdio: 'inherit',
+        env: { 
+            ...process.env, SIMCTL_CHILD_DYLD_INSERT_LIBRARIES: expoDetoxHookFrameworkPath
+        },
+    });
+    process.exit(result.status);
 }
